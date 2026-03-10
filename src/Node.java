a0dba0699ad24b8c767345073db56700f00d5558
No Respect <norespect326@gmail.com>
2026-03-10 02:39:03 +0200

    initialize project

---
+++ src/Node.java
@@ -0,0 +1,7 @@
+public class Node {// node contains
+    // data ( used to store value or item in list)
+    // next (refers to next node (pointer) )
+    int data;
+    Node next;
+
+}
---
+++ src/SingleLinkedList.java
@@ -0,0 +1,24 @@
+public class SingleLinkedList {
+    // linked list must contain a pointer called head
+    // a head used to point to a first node in list, and it's type is Node
+    Node head;
+
+    public void insertFirst(int item) {
+
+    }
+
+    // used to add item at the last index of list
+    // ,and it's a traditional type of inserting
+    public void append(int item) {
+    }
+
+    public void insertAtPosition(int item, int position) {
+    }
+
+    // traverse used to looping on the list and display all items in it
+    public void traverse() {
+    }
+
+    public void delete(int item) {
+    }
+}
